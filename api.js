b12b72ee1a9edb8121453462f1cc69c0f3fb27ab
KimK <KimK@treeskin.ca>
2017-08-14 22:07:21 -0600

    small api.db set up

---
+++ api.js
@@ -0,0 +1,8 @@
+
+
+/**
+ * Connect to node red? Or build own api calls. Ajax?
+ * W/o Node red wont have to worry about portability.
+ */
+
+var baseURL = "";
--- main.js
+++ main.js
@@ -2,6 +2,19 @@ var Main = function(){
 
     main = {};
 
+
+    var populateVisitType = function () {
+        var result = api.getAllVisitTypes();
+    $.when(result).then(function(res){
+        var typeContainer = $("#typesContainer");
+        _.each(res, function(r){
+            var objHTML = "<a href=\"#\">"+ r.name +"</a>"
+            typeContainer.appendTo(objHTML);
+        });
+    });
+
+    }
+
     main.load = function () {
         $('#datePicker').datepicker({
             format: 'mm/dd/yyyy',
