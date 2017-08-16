b637985155f20a7d845d6ffc87ecb38a2d8d6b0a
KimK <KimK@treeskin.ca>
2017-08-15 21:56:45 -0600

    api

--- api.js
+++ api.js
@@ -1,8 +1,28 @@
+var Api = function(){
+    var api = {};
+    /**
+     * Connect to node red? Or build own api calls. Ajax?
+     * W/o Node red wont have to worry about portability.
+     */
+    var apikey = "c6ddf72b-6b5c-4a70-94b5-a7a36eeb2e9c";
+    var baseURL = "https://cloud.mongodb.com/api/atlas/v1.0/groups/Treeskin/clusters/TS-01";
 
-
-/**
- * Connect to node red? Or build own api calls. Ajax?
- * W/o Node red wont have to worry about portability.
- */
-
-var baseURL = "";
+    api.getAllVisitTypes = function(){
+        $.ajax({
+            method : 'GET',
+            url : baseURL,
+            username : "kimberlykroening@hotmail.com",
+            password: apikey,
+            dateType: "json",
+            crossDomain : true,
+            success : function(data){
+                console.log(data);
+                return data;
+            },
+            fail : function (){
+                console.log("error occured");
+            }
+        });
+    }
+    return api;
+}();
--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var Main = function(){
+var Main = function($,api,_){
 
     main = {};
 
 
     var populateVisitType = function () {
         var result = api.getAllVisitTypes();
-    $.when(result).then(function(res){
-        var typeContainer = $("#typesContainer");
-        _.each(res, function(r){
-            var objHTML = "<a href=\"#\">"+ r.name +"</a>"
-            typeContainer.appendTo(objHTML);
+        $.when(result).then(function(res){
+            var typeContainer = $("#typesContainer");
+            _.each(res, function(r){
+                var objHTML = "<a href=\"#\">"+ r.name +"</a>"
+                typeContainer.appendTo(objHTML);
+            });
         });
-    });
-
     }
 
     main.load = function () {
-        $('#datePicker').datepicker({
-            format: 'mm/dd/yyyy',
-            startDate: '-3d'
-            });
+        // $('#datePicker').datepicker({
+        //     format: 'mm/dd/yyyy',
+        //     startDate: '-3d'
+        //     });
+        populateVisitType();
     };
 
     return main;
-}()
+}(jQuery,Api,_)
